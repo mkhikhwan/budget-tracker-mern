0b33d466683d8cf9695f7222da48435b877ea134
mkhikhwan <mkhikhwan@gmail.com>
2026-01-29 16:17:27 +0800

    add overlay when open navbar

--- frontend/src/shared/components/NavBar.tsx
+++ frontend/src/shared/components/NavBar.tsx
@@ -10,40 +10,44 @@ function NavBar(){
     };
 
     return (
-        <nav className={`${styles.navbar} ${isOpen ? styles.open : ''}`}>
-            <div className={styles.logoRow}>
-                <div className={styles.logo}>
-                    <i className="fa-solid fa-money-bill-1-wave"></i> BUDGETNOW
+        <>
+            <div className={`${styles.darkOverlay} ${isOpen ? styles.toggleOverlay : ''}`}></div>
+            <nav className={`${styles.navbar} ${isOpen ? styles.open : ''}`}>
+                <div className={styles.logoRow}>
+                    <div className={styles.logo}>
+                        <i className="fa-solid fa-money-bill-1-wave"></i> BUDGETNOW
+                    </div>
+
+                    <div className={styles.navToggle} onClick={toggleSidebar}>
+                        <i className={`fa-solid ${isOpen ? "fa-angle-left" : "fa-angle-right"}`}></i>
+                    </div>
                 </div>
 
-                <div className={styles.navToggle} onClick={toggleSidebar}>
-                    <i className={`fa-solid ${isOpen ? "fa-angle-left" : "fa-angle-right"}`}></i>
-                </div>
-            </div>
+                <ul className={styles.navList}>
+                    <li className={styles.navItem}>
+                        <Link className={styles.item} to="/">
+                            <i className="fa-solid fa-home"></i> Home
+                        </Link>
+                    </li>
+                    <li className={styles.navItem}>
+                        <Link className={styles.item} to="/dashboard">
+                            <i className="fa-solid fa-chart-line"></i> Dashboard
+                        </Link>
+                    </li>
+                    <li className={styles.navItem}>
+                        <Link className={styles.item} to="/expenses">
+                            <i className="fa-solid fa-receipt"></i> Expenses
+                        </Link>
+                    </li>
+                    <div className={styles.navFooter}>
+                        <button className={styles.logout}>
+                            Logout
+                        </button>
+                    </div>
+                </ul>
+            </nav>
+        </>
 
-            <ul className={styles.navList}>
-                <li className={styles.navItem}>
-                    <Link className={styles.item} to="/">
-                        <i className="fa-solid fa-home"></i> Home
-                    </Link>
-                </li>
-                <li className={styles.navItem}>
-                    <Link className={styles.item} to="/dashboard">
-                        <i className="fa-solid fa-chart-line"></i> Dashboard
-                    </Link>
-                </li>
-                <li className={styles.navItem}>
-                    <Link className={styles.item} to="/expenses">
-                        <i className="fa-solid fa-receipt"></i> Expenses
-                    </Link>
-                </li>
-                <div className={styles.navFooter}>
-                    <button className={styles.logout}>
-                        Logout
-                    </button>
-                </div>
-            </ul>
-        </nav>
     )
 }
 
