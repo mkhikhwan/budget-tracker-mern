12c687592512562867912a2a12dd6f9ff01ec804
mkhikhwan <mkhikhwan@gmail.com>
2026-02-06 10:04:25 +0800

    init db

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,12 +1,16 @@
-import { MongoClient } from "mongodb";
+import { Db, MongoClient } from "mongodb";
 
-const connectDB = async () => {
+let db: Db;
+
+export const connectDB = async () => {
     try{
         const dbUri:string|undefined = process.env.MONGO_URI;
         if(!dbUri) throw new Error("MONGO_URI is not defined"); 
 
         const client = new MongoClient(dbUri);
         await client.connect();
+        db = client.db(process.env.MONGO_DB_NAME);
+
         console.log('MongoDB connected');
     }
     catch(err) {
@@ -15,4 +19,8 @@ const connectDB = async () => {
     }
 };
 
-export default connectDB
+export const getDb = () => {
+    if(!db) throw new Error("Database not initialized");
+
+    return db;
+};
