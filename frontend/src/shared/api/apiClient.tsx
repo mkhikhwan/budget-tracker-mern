44187b0305d1996b0753c7876842314c2cf514a3
mkhikhwan <mkhikhwan@gmail.com>
2026-02-27 15:27:14 +0800

    add formdata support to api calls

--- frontend/src/shared/api/apiClient.tsx
+++ frontend/src/shared/api/apiClient.tsx
@@ -4,9 +4,12 @@ export async function apiClient<T>(
     path: string,
     options: RequestInit = {}
 ): Promise<T>{
+    const isFormData = options.body instanceof FormData;
+
     const res = await fetch(`${BASE_URL}${path}`, {
         headers: {
-            "Content-Type": "application/json",
+            ...(isFormData ? {} : { "Content-Type": "application/json" }),
+            ...options.headers
         },
         ...options,
     });
