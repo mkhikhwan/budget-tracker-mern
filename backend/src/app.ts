e498485af77b830d3b599172d20d4aaa38e9948a
mkhikhwan <mkhikhwan@gmail.com>
2026-03-29 16:03:57 +0800

    Revert "fix: import globalErrorHandler not found"
    This reverts commit 629415fe1c45ba9df6248af0420d33c7849a3fc8.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,7 +3,7 @@ import path from 'path';
 
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import globalErrorHandler from './middleware/GlobalErrorHandler';
+import GlobalErrorHandler from './middleware/GlobalErrorHandler';
 import Auth from './middleware/Auth';
 import morgan from 'morgan'
 
@@ -44,6 +44,6 @@ if(process.env.NODE_ENV === "production"){
 }
 
 // Error Handler Middleware
-app.use(globalErrorHandler);
+app.use(GlobalErrorHandler);
 
 export default app;
